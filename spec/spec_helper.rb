92d9826c51e07f99919b69d9bdcef886f00b4122
Philip Cunningham <hello@filib.io>
2017-08-08 14:08:23 +0100

    update spec helper to require relevant classes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "pirate/service/base"
+require "pirate/service/fetch"
+require "pirate/service/utilities"
 require "pirate/service/version"
