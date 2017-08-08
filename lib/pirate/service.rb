bbc3454505528590400a527276380e6433c386d6
Philip Cunningham <hello@filib.io>
2017-08-08 14:13:46 +0100

    provide single convenience import that exports all classes/modules.

---
+++ lib/pirate/service.rb
@@ -0,0 +1,4 @@
+require "pirate/service/base"
+require "pirate/service/fetch"
+require "pirate/service/utilities"
+require "pirate/service/version"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,2 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "pirate/service/base"
-require "pirate/service/fetch"
-require "pirate/service/utilities"
-require "pirate/service/version"
+require "pirate/service"
